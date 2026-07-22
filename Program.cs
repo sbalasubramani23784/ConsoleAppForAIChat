09c61551d7ea136468128b6893c42a0b0daa8a9a
Balasubramani S <Balasubramani.S@abaxystech.com>
2026-07-22 22:26:26 +0530

    AI Workflow added

--- Program.cs
+++ Program.cs
@@ -1,42 +1,61 @@
+using Microsoft.Agents.AI;
+using Microsoft.Agents.AI.Workflows;
 using Microsoft.Extensions.AI;
 using System.ClientModel;
 
-Console.ForegroundColor = ConsoleColor.Yellow;
-Console.WriteLine("This is bala. My first AI proram.");
 IChatClient chatClient = new OpenAI.Chat.ChatClient(
        "gpt-4.1-mini",
        new ApiKeyCredential("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"),
        new OpenAI.OpenAIClientOptions { Endpoint = new Uri("https://models.github.ai/inference") }
     ).AsIChatClient();
 
-Console.WriteLine("GPT 4.1 mini Chat - type 'exit' to quit");
+
+AIAgent TamilAgent  = new ChatClientAgent(
+    chatClient, 
+    new ChatClientAgentOptions { Name = "TamilAgent", ChatOptions = new ChatOptions { Instructions = "You are a translation assistant that the provided text to Tamil." } }
+    );
+
+AIAgent HindiAgent = new ChatClientAgent(
+    chatClient,
+    new ChatClientAgentOptions { Name = "HindiAgent", ChatOptions = new ChatOptions { Instructions = "You are a translation assistant that the provided text to Hindi." } }
+    );
+
+string QualityReviewerInstruction = """
+        You are a multilingual translation quality reviewer.
+    Check the translations for grammar accuracy, tone consistency, and cultural fit
+    compared to the original English text.
+
+    Give a brief summary with a quality rating (Excellent / Good / Need a Review).
+
+    Example output:
+    Quality: Excellent
+    Feedback: Accurate translation, friendly tone preserved, minor punctuation tweaks only.
+
+    """;
+
+AIAgent ReviewerAgent = new ChatClientAgent(
+    chatClient,
+    new ChatClientAgentOptions { Name = "ReviewerAgenet", ChatOptions = new ChatOptions { Instructions = QualityReviewerInstruction } }
+    );
+
+AIAgent workflowAgent = AgentWorkflowBuilder.BuildSequential(TamilAgent, HindiAgent,ReviewerAgent).AsAIAgent();
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("You: ");
+string userInput = Console.ReadLine() ?? "Welcome to our application. Please Verify you email address before continuing.";
+
+
+AgentResponse agentRunResponse = await workflowAgent.RunAsync(userInput);
+
 Console.WriteLine();
-List<Microsoft.Extensions.AI.ChatMessage> messages = new();
 
-while(true){
-    Console.ForegroundColor = ConsoleColor.White;
-    Console.WriteLine("You :");
-    var userInput = Console.ReadLine();
-
-    if (string.IsNullOrEmpty(userInput)) continue;
-    if(userInput.Equals("exit",StringComparison.OrdinalIgnoreCase)) break;
+foreach(var message in agentRunResponse.Messages)
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine($"{message.AuthorName}:");
 
     Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine("Assitent:");
-    messages.Add(new ChatMessage(ChatRole.User, userInput));
-    string assistentMessage = GetResponse(chatClient.GetStreamingResponseAsync(messages)).Result;                
-    messages.Add(new ChatMessage(ChatRole.Assistant, assistentMessage));
+    Console.WriteLine(message.Text);
+
     Console.WriteLine();
-}
-
-async static Task<string> GetResponse(IAsyncEnumerable<ChatResponseUpdate> messages)
-{
-    string assistentMessage = string.Empty;
-    await foreach (var res in messages)
-    {
-        Console.WriteLine(res.Text);
-        assistentMessage += res.Text;
-    }
-
-    return assistentMessage;
 }
